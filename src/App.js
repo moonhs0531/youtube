d928d0291213be8f1fbbe9f1b541ec6b33378637
moon <moonhs0531@naver.com>
2021-12-18 23:41:24 +0900

    GlobalStyle(reset css)적용

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import styled from 'styled-components';
 import Routes from './Routes';
 import HeaderContainer from './shared/header/containers/HeaderContainer';
 import GnbContainer from './shared/gnb/containers/GnbContainer';
+import { GlobalStyle } from './style/GlobalStyle';
 
 const App = function () {
   return (
     <Container>
+      <GlobalStyle />
       <HeaderContainer />
       <GnbContainer />
       <Routes />
